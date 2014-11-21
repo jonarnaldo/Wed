1ab43ab658cabcb92b9472299078491d20500302
jonarnaldo <jonathanarnaldo@Jonathans-MacBook-Air.local>
2014-11-20 21:46:57 -0800

    fixed routes

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 
 // BASE SETUP
 var express = require('express')
-var port = process.env.PORT || 5000
 var app = express();
+var router = express.Router();
+var port = process.env.PORT || 5000
+var path = require('path');
+var pub = __dirname;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function(request, response) {
-//   response.send('Hello World!')
-// })
+app.set('views', pub);
 
+app.set('view engine', 'jade');
 
-// ROUTES
-var home = require('./routes/index');
-
-// ASSIGN ROUTES
-app.use('/', home);
+app.get('/', function(req,res){
+  res.render('./views/index', { title: "hey" });
+});
 
 // catch 404
 app.use(function(req, res, next) {
@@ -23,6 +23,7 @@ app.use(function(req, res, next) {
   error.status = 404;
   next(error);
 })
+
 
 // development error handler
 // will print stacktrace
@@ -46,12 +47,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
-// VIEW ENGINE SET UP
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.listen(port, function(req, res) {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + port)
 })
 
 module.exports = app;
